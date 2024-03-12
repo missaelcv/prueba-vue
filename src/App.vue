b7cb96401450cda6905885b66f41b489578e7f8a
missaelcv <missaelcv0220@gmail.com>
2024-03-12 12:17:23 -0400

    Ramas

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Ejmplo para 55to!" />
+      <HelloWorld msg="Ejmplo para Prueba de ramas" />
     </div>
   </header>
 
